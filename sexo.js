e17534321173663b5209f1ab21ca90c3255a52d7
RodrigoCervantes17 <99071743+RodrigoCervantes17@users.noreply.github.com>
2023-10-15 01:09:16 -0600

    added html to interact and check if everything works
    yeah that's it

--- sexo.js
+++ sexo.js
@@ -1,55 +1,55 @@
 
-class Bus {
-  constructor(nombre, minutos) {
-    this.siguiente = null;
-    this.anterior = null;
-    this.nombre = nombre;
-    this.minutos = minutos;
-  }
-  infoHTML() {
-    return `
-        <div class="producto">
-            <h1>${this.nombre}</h1>
-            <p>${this.minutos}</p>
-        </div>
-        `;
-  }
-}
-
-class Ruta {
-  constructor() {
-    this.primero = null;
-  }
-  agregar(nuevo) {
-    if (this.primero == null) {
-        this.primero = nuevo;
-        this.primero.siguiente = this.primero // Este pedo pa hacerlo circular
-        this.primero.anterior = this.primero
-    }else { 
-        aux = this.primero
-        while(aux.siguiente !== this.primero){
-            aux = aux.siguiente
-        }
-    aux.siguiente = nuevo;
-    nuevo.anterior = aux;
-    nuevo.siguiente = this.primero;
-    this.primero.anterior = nuevo;
+  class Bus {
+    constructor(nombre, minutos) {
+      this.siguiente = null;
+      this.anterior = null;
+      this.nombre = nombre;
+      this.minutos = minutos;
+    }
+    infoHTML() {
+      return `
+          <div class="bus">
+              <h1>${this.nombre}</h1>
+              <p>${this.minutos}</p>
+          </div>
+          `;
     }
   }
-  listar() {
-    let lista=""
-    let aux = this.primero
-    while(aux.siguiente!==this.primero){
-      lista += aux.infoHTML()
-      aux=aux.siguiente
-    }
-    lista += aux.infoHTML();
-    return lista;
 
+  class Ruta {
+    constructor() {
+      this.primero = null;
+    }
+    agregar(nuevo) {
+      if (this.primero == null) {
+          this.primero = nuevo;
+          this.primero.siguiente = this.primero // Este pedo pa hacerlo circular
+          this.primero.anterior = this.primero
+      }else { 
+          aux = this.primero
+          while(aux.siguiente !== this.primero){
+              aux = aux.siguiente
+          }
+      aux.siguiente = nuevo;
+      nuevo.anterior = aux;
+      nuevo.siguiente = this.primero;
+      this.primero.anterior = nuevo;
+      }
+    }
+    listar() {
+      let lista=""
+      let aux = this.primero
+      while(aux.siguiente!==this.primero){
+        lista += aux.infoHTML()
+        aux=aux.siguiente
+      }
+      lista += aux.infoHTML();
+      console.log(lista);
+
+    }
+    buscar() {}
+    eliminar() {}
+    listarInverso() {}
+    agregarInicio() {}
+    crearRuta(baseInicio, horaInicio, horaFin) {}
   }
-  buscar() {}
-  eliminar() {}
-  listarInverso() {}
-  agregarInicio() {}
-  crearRuta(baseInicio, horaInicio, horaFin) {}
-}
